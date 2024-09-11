3e874b81c91c2dc6e30c3c1db22495c873ddd748
qwq140 <qwq140@gmail.com>
2024-09-11 17:35:46 +0900

    feat : 게시글 상세 오류메시지 다이얼로그로 표시

--- src/pages/Post/components/PostDetailBody.js
+++ src/pages/Post/components/PostDetailBody.js
@@ -4,11 +4,11 @@ import api from "../../../api/api";
 import CustomButton from "../../../components/CustomButton";
 import {useAuth} from "../../../context/AuthContext";
 import CommentList from "./CommentList";
+import ErrorDialog from "../../../components/ErrorDialog";
 
 const PostDetailBody = ({postId}) => {
     const navigate = useNavigate();
     const [post, setPost] = useState(null); // 게시글 상태
-    const [loading, setLoading] = useState(true); // 로딩 상태
     const [error, setError] = useState(null); // 오류 상태
 
     const {state : authState} = useAuth();
@@ -19,18 +19,14 @@ const PostDetailBody = ({postId}) => {
 
         const fetchPost = async () => {
             try {
-                const response = await api.get(`/posts/${postId}`); // 게시글 데이터 가져오기
+                const response = await api.get(`/posts/${postId}`);
                 if(!ignore) {
                     setPost(response.data.data);
                 }
             } catch (err) {
-                if(err.status === 404) {
-                    setPost(null);
-                } else {
-                    setError('게시글을 불러오는 중 오류가 발생했습니다.'); // 오류 메시지 설정
+                if(err.status !== 404) {
+                    setError('게시글을 불러오는 중 오류가 발생했습니다.');
                 }
-            } finally {
-                setLoading(false); // 로딩 상태 종료
             }
         };
 
@@ -52,16 +48,9 @@ const PostDetailBody = ({postId}) => {
         }
     };
 
-    if (loading) {
-        return <div>로딩 중...</div>;
-    }
-
-    if (error) {
-        return <div>{error}</div>;
-    }
-
     return (
         <div className='p-4'>
+            <ErrorDialog message={error} onClose={() => setError(null)}/>
             {post ? (
                 <>
                     <h1 className='text-2xl font-bold mb-4'>{post.title}</h1>
