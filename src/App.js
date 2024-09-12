85a0a0ecc00bc8fbefd0d2ad9b37632b2872f34d
qwq140 <qwq140@gmail.com>
2024-09-12 15:33:47 +0900

    feat : axios interceptor hook

--- src/App.js
+++ src/App.js
@@ -12,45 +12,55 @@ import PostDetail from "./pages/Post/PostDetail";
 import PostEdit from "./pages/Post/PostEdit";
 import BoardCreate from "./pages/Board/BoardCreate";
 import BoardManagement from "./pages/Board/BoardManagement";
+import {useAxiosInterceptors} from "./api/api";
+import {useEffect} from "react";
 
 function App() {
     return (
         <AuthProvider>
-            <BrowserRouter>
-                <div className="flex flex-col min-h-screen">
-                    <Header/>
-                    <div className="flex flex-grow bg-gray-200">
-                        <Routes>
-                            <Route path="/" element={<BoardList/>}/>
-                            <Route path="/board/:boardId" element={<BoardDetail/>}/>
-                            <Route path="/signup" element={<Signup/>}/>
-                            <Route path="/login" element={<Login/>}/>
-                            <Route path="/board/:boardId/post/write" element={
-                                <ProtectedRoute>
-                                    <PostWrite/>
-                                </ProtectedRoute>
-                            }/>
-                            <Route path="/post/:postId/edit" element={
-                                <ProtectedRoute>
-                                    <PostEdit/>
-                                </ProtectedRoute>
-                            }/>
-                            <Route path="/post/:postId" element={<PostDetail/>}/>
-                            <Route path="/board/create" element={
-                                <ProtectedRoute requiredRole="ADMIN">
-                                    <BoardCreate/>
-                                </ProtectedRoute>
-                            }/>
-                            <Route path="/board/management" element={
-                                <ProtectedRoute requiredRole="ADMIN">
-                                    <BoardManagement/>
-                                </ProtectedRoute>
-                            }/>
-                        </Routes>
-                    </div>
-                </div>
-            </BrowserRouter>
+            <MainComponent/>
         </AuthProvider>
+    );
+}
+
+function MainComponent() {
+    useAxiosInterceptors();
+
+    return (
+        <BrowserRouter>
+            <div className="flex flex-col min-h-screen">
+                <Header/>
+                <div className="flex flex-grow bg-gray-200">
+                    <Routes>
+                        <Route path="/" element={<BoardList/>}/>
+                        <Route path="/board/:boardId" element={<BoardDetail/>}/>
+                        <Route path="/signup" element={<Signup/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                        <Route path="/board/:boardId/post/write" element={
+                            <ProtectedRoute>
+                                <PostWrite/>
+                            </ProtectedRoute>
+                        }/>
+                        <Route path="/post/:postId/edit" element={
+                            <ProtectedRoute>
+                                <PostEdit/>
+                            </ProtectedRoute>
+                        }/>
+                        <Route path="/post/:postId" element={<PostDetail/>}/>
+                        <Route path="/board/create" element={
+                            <ProtectedRoute requiredRole="ADMIN">
+                                <BoardCreate/>
+                            </ProtectedRoute>
+                        }/>
+                        <Route path="/board/management" element={
+                            <ProtectedRoute requiredRole="ADMIN">
+                                <BoardManagement/>
+                            </ProtectedRoute>
+                        }/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
     );
 }
 
--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {useAuth} from "../context/AuthContext";
 
 const API_BASE_URL = "http://localhost:8080/api";
 
@@ -9,49 +10,60 @@ const api = axios.create({
     },
 });
 
-api.interceptors.request.use((config) => {
-   const token = localStorage.getItem('accessToken');
-   if(token) {
-       config.headers.Authorization = `Bearer ${token}`;
-   }
-   return config;
-});
 
-api.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    async (error) => {
-        if(error.response && error.response.status === 401) {
-            const refreshToken = localStorage.getItem("refreshToken");
-            if(refreshToken) {
-                try {
-                    const response = await api.post("/auth/reissue", {refreshToken});
-                    const newAccessToken = response.data.data.accessToken;
-                    const newRefreshToken = response.data.data.refreshToken;
+const handleUnauthorized = async (error, originalRequest, authDispatch) => {
+    const refreshToken = localStorage.getItem("refreshToken");
+    if(refreshToken) {
+        try {
+            const response = await api.post("/auth/reissue", {refreshToken});
+            const newAccessToken = response.data.data.accessToken;
+            const newRefreshToken = response.data.data.refreshToken;
 
-                    localStorage.setItem("accessToken", newAccessToken);
-                    localStorage.setItem("refreshToken", newRefreshToken);
+            localStorage.setItem("accessToken", newAccessToken);
+            localStorage.setItem("refreshToken", newRefreshToken);
 
-                    error.config.headers.Authorization = `Bearer ${newAccessToken}`;
-                } catch (e) {
-                    console.log('토큰 갱신 실패 : ', e);
+            originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+            return api(originalRequest);
+        } catch (e) {
+            authDispatch({type:"LOGOUT"});
 
-                    localStorage.removeItem("accessToken");
-                    localStorage.removeItem("refreshToken");
-                    localStorage.removeItem("isAuth");
-                    localStorage.removeItem("user");
-
-                    alert("로그인을 다시 해주세요");
-                    window.location.href = '/login';
-                }
-            } else {
-                alert("로그인을 해주세요");
-                window.location.href = "/login";
-            }
+            alert("로그인을 다시 해주세요");
+            window.location.href = '/login';
         }
-        return Promise.reject(error);
+    } else {
+        authDispatch({type:"LOGOUT"});
+
+        alert("로그인을 해주세요");
+        window.location.href = "/login";
     }
-);
+}
+
+export const useAxiosInterceptors = () => {
+    const {dispatch} = useAuth();
+
+    api.interceptors.request.use((config) => {
+        const token = localStorage.getItem('accessToken');
+        if(token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    });
+
+    api.interceptors.response.use(
+        (response) => {
+            return response;
+        },
+        async (error) => {
+            const originalRequest = error.config;
+            if(error.response && error.response.status === 401 && !originalRequest._retry) {
+                originalRequest._retry = true;
+                return handleUnauthorized(error, originalRequest, dispatch);
+            }
+            return Promise.reject(error);
+        }
+    );
+}
+
+
 
 export default api;
